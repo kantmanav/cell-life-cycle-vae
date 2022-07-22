c75d773b5a2bf18ba3ad5aaea688b97f00a6989d
Manav Kant <kantmanav@gmail.com>
2022-07-21 17:55:14 -0700

    Get reconstructions.

--- docker-run.sh
+++ docker-run.sh
@@ -1,7 +1,7 @@
 docker run -it \
 --gpus "device=2" \
--p 94:8888 \
+-p 96:8888 \
 -v $PWD:/cell-life-cycle-vae \
 -v /deepcell_data/users/manav:/data \
 -v /home/mkant/data-registry/data/training:/training \
-$USER/cell-life-cycle-vae:0.12.0
+$USER/cell-life-cycle-vae:0.12.2
