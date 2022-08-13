13c7cf914e62df3ea8164f02dd9c1cab453d993c
Manav Kant <kantmanav@gmail.com>
2022-08-13 12:56:47 -0700

    Prepare for transfer to Havok.

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,6 @@
 docker run -it \
 --gpus "device=2" \
--p 96:8888 \
+-p 94:8888 \
 -v $PWD:/cell-life-cycle-vae \
 -v /deepcell_data/users/manav:/data \
 -v /home/mkant/data-registry/data/training:/training \
