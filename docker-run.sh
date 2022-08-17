fe4d16dfdbb478787c6c9562b035b72a769ce3a9
kantmanav <kantmanav@gmail.com>
2022-08-17 16:19:42 -0700

    Organize data loading and VAE training code.

--- docker-run.sh
+++ docker-run.sh
@@ -1,7 +1,6 @@
 docker run -it \
---gpus "device=2" \
--p 94:8888 \
+--gpus "device=3" \
+-p 93:8888 \
 -v $PWD:/cell-life-cycle-vae \
--v /deepcell_data/users/manav:/data \
--v /home/mkant/data-registry/data/training:/training \
+-v /deepcell_data/users/manav/summer_2022:/data \
 $USER/cell-life-cycle-vae:0.12.2
