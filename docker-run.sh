c5974286d59942c2129a62bf260ce4f30e68e6cc
kantmanav <kantmanav@gmail.com>
2022-09-01 17:52:23 -0700

    Get axis traversal images.

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,6 @@
 docker run -it \
---gpus "device=1" \
--p 91:8888 \
+--gpus "device=3" \
+-p 93:8888 \
 -v $PWD:/cell-life-cycle-vae \
 -v /deepcell_data/users/manav/summer_2022:/data \
 $USER/cell-life-cycle-vae:0.12.2
