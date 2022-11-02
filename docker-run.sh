5d38b0eca960372ace51a93f1a1a5f34de407960
Manav Kant <kantmanav@gmail.com>
2022-11-01 22:48:02 -0700

    Start debugging normalization.

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,6 @@
 docker run -it \
---gpus "device=3" \
--p 99:8888 \
+--gpus "device=2" \
+-p 92:8888 \
 -v $PWD:/cell-life-cycle-vae \
--v ~/deepcell_data/users/manav/summer_2022:/data \
+-v ~/data/users/manav/summer_2022:/data \
 $USER/cell-life-cycle-vae:0.12.2
