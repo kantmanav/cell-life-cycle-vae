6796b3a076449c6a47210f83a793754c2696fafe
Manav Kant <kantmanav@gmail.com>
2022-12-17 14:08:13 -0800

    Test other VAE methodology.

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,7 @@
 docker run -it \
 --gpus "device=2" \
--p 92:8888 \
+-p 93:8888 \
 -v $PWD:/cell-life-cycle-vae \
 -v ~/data/users/manav/summer_2022:/data \
+-v ~/tf_records:/tf_records \
 $USER/cell-life-cycle-vae:0.12.2
