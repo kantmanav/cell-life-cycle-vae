6cf509b274ad772d600c654f15137ddc77ad2225
Manav Kant <kantmanav@gmail.com>
2023-01-27 23:54:32 -0800

    Prepare to write big image dataset.

--- docker-run.sh
+++ docker-run.sh
@@ -2,6 +2,7 @@ docker run -it \
 --gpus "device=2" \
 -p 93:8888 \
 -v $PWD:/cell-life-cycle-vae \
--v ~/data/users/manav/summer_2022:/data \
+-v ~/tracking-nuclear:/data \
+-v ~/vae_models:/models \
 -v ~/tf_records:/tf_records \
 $USER/cell-life-cycle-vae:0.12.2
